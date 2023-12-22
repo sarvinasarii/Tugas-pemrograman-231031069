c3e11db40d207b9196a7207dedd0f6505097c23f
sarvinasarii <153784006+sarvinasarii@users.noreply.github.com>
2023-12-22 10:57:23 +0800

    Add files via upload

---
+++ 231031069-sarvina sari-tugas-9-C.py
@@ -0,0 +1,20 @@
+biodata = {'nama'  : 'Sarvina Sari',
+           'nim'   : 231031069,
+           'prodi' : 'Sistem Informasi C',
+           'umur'  : 18,
+           'hobi'  : 'Bermain Basket',
+           'alamat': 'Jalan Menara',
+           'asal'  : 'Parepare',
+           'status': 'Mahasiswa Aktif Institut Teknologi B.J Habibie'
+           }
+
+print(f'''
+Nama Saya  : {biodata['nama']}
+Dengan NIM : {biodata['nim']}
+Dari Prodi : {biodata['prodi']}
+Umur Saya  : {18}
+Hobi Saya  : {biodata['hobi']}
+Alamat Saya: {biodata['alamat']}
+Asal Saya  : {biodata['asal']}
+Status     : {biodata['status']}
+''')
